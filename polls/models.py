fc6c0597b451af376ce129c4ae9ef8df76c17799
Kim-Hyun-sung <rlagustjd159@gmail.com>
2022-09-30 13:55:10 +0900

    0930_2시

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Question(models.Model): # DB Table for 설문조사 주제
+    question_text = models.CharField(max_length=200) # 설문조사 주제 텍스트
+    pub_date = models.DateTimeField('date published') # ‘date published’ : 관리자 페이지에서 보여질 항목명
+    
+    def __str__(self): # Admin page에서 display되는 텍스트에 대한 변경
+        return f"{self.pk}번\n 글: {self.question_text} \n 날짜 :{self.pub_date}"
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+
+class Choice(models.Model): # DB Table for 설문조사 주제별 선택지 (+ 선택지마다의 득표 수)
+    # 자동으로 Question table의 Primary key를 Foreign Key로 세팅
+    # on_delete=models.CASCADE : Question(질문) 항목 삭제 시 관계된 선택지들도 모두 자동 삭제
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # 설문조사 주제의 id 값
+    choice_text = models.CharField(max_length=200) # 설문조사 주제에 대한 선택지 텍스트
+    votes = models.IntegerField(default=0) # 해당 선택지의 득표 수
+    
+    def __str__(self): # Admin page에서 display되는 텍스트에 대한 변경
+        return f"{self.pk}번\n 질문: {self.question} \n 글  :{self.choice_text} \n 투표 :{self.votes}"
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,25 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    # order_by('-pub_date')[:5] : 등록 날짜 기준 내림차순 정렬 후 앞에서 5개까지만
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] 
+    # 지난 실습에서 render() 함수의 {~:~} 로 html 파일에게 넘겨주던 dict를 context라고 부릅니다.
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context) 
+
+
+def detail(request, question_id):
+    q = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question':q})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.".format(question_id))
